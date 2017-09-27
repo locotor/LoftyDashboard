651a5ef6cdfd60ab43594d291efc752ed1ac509e
locotor <locotor@outlook.com>
2017-09-27 18:18:04 +0800

    update

--- src/areas/dashboard/chart/chart.component.ts
+++ src/areas/dashboard/chart/chart.component.ts
@@ -6,7 +6,7 @@ import { Component, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
-  constructor(el: ElementRef) { 
+  constructor(private el: ElementRef) { 
     console.log(el.nativeElement);
   }
 
@@ -17,128 +17,127 @@ export class ChartComponent implements OnInit {
       'insideTop', 'insideLeft', 'insideRight', 'insideBottom',
       'insideTopLeft', 'insideTopRight', 'insideBottomLeft', 'insideBottomRight'
     ];
-    console.log()
-    let dom = document.getElementById("charts");
-    let myChart = echarts.init(dom);
-    // let option = {
-    //   color: ['#3398DB'],
-    //   tooltip: {
-    //     trigger: 'axis',
-    //     axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-    //       type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-    //     }
-    //   },
-    //   grid: {
-    //     left: '3%',
-    //     right: '4%',
-    //     bottom: '3%',
-    //     containLabel: true
-    //   },
-    //   xAxis: [
-    //     {
-    //       type: 'category',
-    //       data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    //       axisTick: {
-    //         alignWithLabel: true
-    //       }
-    //     }
-    //   ],
-    //   yAxis: [
-    //     {
-    //       type: 'value'
-    //     }
-    //   ],
-    //   series: [
-    //     {
-    //       name: '直接访问',
-    //       type: 'bar',
-    //       barWidth: '60%',
-    //       data: [10, 52, 200, 334, 390, 330, 220]
-    //     }
-    //   ]
-    // };
-    // myChart.setOption(option);
+    let componentEl = this.el.nativeElement;
+    let myChart = echarts.init(componentEl.firstChild.childNodes[1]);
+    let option = {
+      color: ['#3398DB'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '直接访问',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
+        }
+      ]
+    };
+    myChart.setOption(option);
   }
 
-  single = [
-    {
-      "name": "11月",
-      "value": 8940000
-    },
-    {
-      "name": "10月",
-      "value": 5000000
-    },
-    {
-      "name": "9月",
-      "value": 7200000
-    }
-  ];
-  multi = [
-    {
-      "name": "villas",
-      "series": [
-        {
-          "name": "9月",
-          "value": 6300000
-        },
-        {
-          "name": "10月",
-          "value": 6800000
-        },
-        {
-          "name": "11月",
-          "value": 7300000
-        },
-        {
-          "name": "12月",
-          "value": 8940000
-        }
-      ]
-    },
+  // single = [
+  //   {
+  //     "name": "11月",
+  //     "value": 8940000
+  //   },
+  //   {
+  //     "name": "10月",
+  //     "value": 5000000
+  //   },
+  //   {
+  //     "name": "9月",
+  //     "value": 7200000
+  //   }
+  // ];
+  // multi = [
+  //   {
+  //     "name": "villas",
+  //     "series": [
+  //       {
+  //         "name": "9月",
+  //         "value": 6300000
+  //       },
+  //       {
+  //         "name": "10月",
+  //         "value": 6800000
+  //       },
+  //       {
+  //         "name": "11月",
+  //         "value": 7300000
+  //       },
+  //       {
+  //         "name": "12月",
+  //         "value": 8940000
+  //       }
+  //     ]
+  //   },
 
-    {
-      "name": "hotel",
-      "series": [
-        {
-          "name": "9月",
-          "value": 7300000
-        },
-        {
-          "name": "10月",
-          "value": 7500000
-        },
-        {
-          "name": "11月",
-          "value": 7870000
-        },
-        {
-          "name": "12月",
-          "value": 8270000
-        }
-      ]
-    },
+  //   {
+  //     "name": "hotel",
+  //     "series": [
+  //       {
+  //         "name": "9月",
+  //         "value": 7300000
+  //       },
+  //       {
+  //         "name": "10月",
+  //         "value": 7500000
+  //       },
+  //       {
+  //         "name": "11月",
+  //         "value": 7870000
+  //       },
+  //       {
+  //         "name": "12月",
+  //         "value": 8270000
+  //       }
+  //     ]
+  //   },
 
-    {
-      "name": "apartment",
-      "series": [
-        {
-          "name": "9月",
-          "value": 7300000
-        },
-        {
-          "name": "10月",
-          "value": 6300000
-        },
-        {
-          "name": "11月",
-          "value": 8000002
-        },
-        {
-          "name": "12月",
-          "value": 7800000
-        }
-      ]
-    }
-  ];
+  //   {
+  //     "name": "apartment",
+  //     "series": [
+  //       {
+  //         "name": "9月",
+  //         "value": 7300000
+  //       },
+  //       {
+  //         "name": "10月",
+  //         "value": 6300000
+  //       },
+  //       {
+  //         "name": "11月",
+  //         "value": 8000002
+  //       },
+  //       {
+  //         "name": "12月",
+  //         "value": 7800000
+  //       }
+  //     ]
+  //   }
+  // ];
 }
